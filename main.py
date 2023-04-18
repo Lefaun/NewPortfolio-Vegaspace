e1a9fac3ad3b93cbee01a1f1d40433139b66a70f
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-04-18 21:45:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def display_menu():
         st.title("Exemple de Pequeno Jogo em Python e Blender")
         st.write("Jogo 3D em UPBGE")
 
-        components.iframe("https://cdn.soft8soft.com/AROAJSY2GOEHMOFUVPIOE:85cf9c6521/Sub%20Python%20Game/Sub%20Python%20Game.html", width=800, heigth =600)
+        components.iframe("https://cdn.soft8soft.com/AROAJSY2GOEHMOFUVPIOE:85cf9c6521/Sub%20Python%20Game/Sub%20Python%20Game.html", width=800, height =600)
         st.video("https://youtu.be/ZfxPMMzyeX8")
        
    
