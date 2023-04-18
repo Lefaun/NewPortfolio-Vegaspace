a6657970861f2dec3827617679559085a76df483
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-04-18 21:57:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,6 @@ def display_menu():
         with col2:
             st.header("3D Animation")
             st.image("image5.jpeg")
-            s
 
             button_2 = st.button("Gosto", key="asian")
         with col3:
@@ -156,16 +155,14 @@ def display_menu():
             st.header("3D ART")
             st.image("Carrinha.jpeg")
             button_3 = st.button("Gosto", key="psy ")
-        col1, col2, col3 = st.columns(3)
-
-        with col1:
+        col1, col2 = st.columns(2)
             
-        with col2:
+        with col1:
             st.header("Design")
             st.image("2.png")
 
             button_2 = st.button("Gosto", key="asian")
-        with col3:
+        with col2:
             st.header("3D ART")
             st.image("Carrinha.jpeg")
             button_3 = st.button("Gosto", key="cutr ")
