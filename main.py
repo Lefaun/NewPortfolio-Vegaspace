8546569d354a4ba570c8f961a4dae1275ca14c97
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-05-10 22:54:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ with st.sidebar:
         with st.expander("Formulário de Contacto"):
             emailsubject = email_form.text_input (label = ' Escreva aqui o Assunto ')
             emailmessage = email_form.text_area (label = ' Escreva a sua Mensagem ')
-            email_form.button(label=' Enviar ')
+            email_form.form_submit_button()(label=' Enviar ')
 
-            if email_form.button:
+            if email_form.form_submit_button():
                 send_mail()
                 st.subheader('  Mensagem enviada com Sucesso!') 
                     
