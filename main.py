daf1f19a53bef7806aab72c6793d2391e13734af
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-05-10 21:39:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -469,4 +469,4 @@ def send_mail():
         server.sendmail(username, to_email, email, emailsubject, emailmessage)
         server.close()
         st.success('e-mail enviado com sucesso')
-display_menu()      
+    display_menu()      
