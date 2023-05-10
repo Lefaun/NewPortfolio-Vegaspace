cf494580f731e13b5420b75c90f3f66bac48de22
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-05-10 22:10:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -102,10 +102,10 @@ with st.sidebar:
         with st.expander("Formulário de Contacto"):
             emailsubject = email_form.text_input (label = ' Escreva aqui o Assunto ')
             emailmessage = email_form.text_area (label = ' Escreva a sua Mensagem ')
-            submit_e_button = st.form_submit_button(label='Enviar', on_click= send_mail())
+            st.form_submit_button(label='Enviar', on_click= send_mail())
              #email_form.form_submit_button(label='Enviar', on_click= send_mail())
 
-            if submit_e_button:
+            if st.form_submit_button:
                     st.subheader('  Mensagem enviada com Sucesso!') 
                     send_mail()
             # Create the responsive menu
