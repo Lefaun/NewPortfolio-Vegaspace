dc4fbbace235f6a8f271158022872b7f0a5f1897
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-05-11 09:34:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -93,9 +93,6 @@ def send_mail():
         password = 'kmfnhlaiasjvxvyw'
         server.login(username, password)
         to_email = 'maillefaun@gmail.com'
-        subject = str()
-        email = str()
-        message = str()
         server.sendmail(username, to_email, email, subject, message)
         server.close()
     except Exception as e:
