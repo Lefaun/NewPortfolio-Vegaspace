6c629cea2748828d6a61a51fd46cbec396af7208
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-06-12 14:47:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,74 @@ from tensorflow.keras.applications.resnet_v2 import ResNet50V2, preprocess_input
 import numpy as np
 import pandas as pd
 import csv
+###########################
+st.set_page_config(
+    page_title="Le Faun's Portfolio",
+    layout="wide"
+)
+
+# Define the CSS to hide Streamlit UI elements and set the background iframe
+custom_css = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    .stApp {
+        background: transparent;
+    }
+    .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        z-index: -1;
+        overflow: hidden;
+    }
+    .foreground {
+        position: relative;
+        z-index: 1;
+    }
+    .iframe {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        border: none;
+        z-index: -1;
+    }
+    @media (max-width:768px) {
+        .foreground {
+            padding: 10px;
+            align-items: center;
+            color: white;
+        }
+    }
+    .stApp {
+        foreground: orange;
+    }
+    </style>
+"""
+
+# HTML to embed the OpenProcessing iframe
+openprocessing_iframe = """
+    <div class="background">
+        <iframe src="https://openprocessing.org/sketch/2257597/embed/" width="100%" height="100%"></iframe>
+    </div>
+"""
+
+# Inject the CSS and HTML into the Streamlit app
+st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown(openprocessing_iframe, unsafe_allow_html=True)
+
+# Wrap your Streamlit content inside a div with the 'foreground' class
+st.markdown('<div class="foreground">', unsafe_allow_html=True)
+
+################################################
+
+
+
+
 main_bg = "lightgreen.jpeg"
 main_bg_ext = "jpeg"
 
@@ -471,6 +539,7 @@ def display_menu():
             tooltip=['label', 'prob'])
 
             st.altair_chart(c, use_container_width=True)
+st.markdown('</div>', unsafe_allow_html=True)
 display_menu()   
 
 
